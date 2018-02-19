f86e52273f2612c9767c6a31fd613ed49eb5b243
SophaktraNet <sophaktranet@gmail.com>
2018-02-19 09:31:39 +0700

    This is the sample project of Movie MVC.

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "This is <h1>Hello World Page!</h1>";
+        //}
+
+        //public string MainPage()
+        //{
+        //    return "This is <strong> Main Page </strong>";
+        //}
+    }
+}
